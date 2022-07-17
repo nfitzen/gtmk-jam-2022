f456a3b26cd6d9d1900eb45aa6ea8f15d17fc464
Nathaniel Fitzenrider <nathaniel@fitzenrider.com>
2022-07-16 21:13:53 -0400

    4 tab supremacy gang
    ...sorry

--- src/instances/scenes/level/Level.gd
+++ src/instances/scenes/level/Level.gd
@@ -6,69 +6,70 @@ extends Node2D
 enum {UP, RIGHT, DOWN, LEFT}
 
 const DELTAS = [
-	Vector2(0, 1),
-	Vector2(1, 0),
-	Vector2(0, -1),
-	Vector2(-1, 0)
+    Vector2(0, 1),
+    Vector2(1, 0),
+    Vector2(0, -1),
+    Vector2(-1, 0)
    ]
 
 enum {
-	BLACK_TILE = 0
-	WHITE_TILE = 1
-	}
+    BLACK_TILE = 0
+    WHITE_TILE = 1
+    }
 
 export var grid_size = 4
 export var n_steps = 8
 
 onready var TileMap = get_node("TileMap")
+onready var Camera = get_node("Camera2D")
 
 func in_bounds(vec, size):
-	return vec.x >= 0 and vec.y >= 0 and vec.x < size and vec.y < size
+    return vec.x >= 0 and vec.y >= 0 and vec.x < size and vec.y < size
 
 var die_grid_pos = Vector2(0, 0)
 
 var grid = []
 
 class MutableDieState:
-	var top = 1
-	var bottom = 6
-	var sides = [5,4,2,3]
-	
-	func move(direction):
-		var old_top = top
-		top = sides[direction]
-		sides[direction] = bottom
-		bottom = sides[direction^2]
-		sides[direction^2] = old_top
+    var top = 1
+    var bottom = 6
+    var sides = [5,4,2,3]
+
+    func move(direction):
+        var old_top = top
+        top = sides[direction]
+        sides[direction] = bottom
+        bottom = sides[direction^2]
+        sides[direction^2] = old_top
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	initialize_grid(grid_size, n_steps)
-	
+    initialize_grid(grid_size, n_steps)
+
 func debug_print_grid():
-	for row in grid:
-		print(row)
-	print()
+    for row in grid:
+        print(row)
+    print()
 
 func initialize_grid(grid_size, n_steps):
-	for y in range(grid_size):
-		var row = []
-		for x in range(grid_size):
-			row.append(0)
-			TileMap.set_cell(x, y, BLACK_TILE)
-		grid.append(row)
-	var initializer_die = MutableDieState.new()
-	var initializer_die_grid_pos = Vector2(0, 0)
-	for _i in range(n_steps):
-		var direction = randi() % 4
-		while not in_bounds(initializer_die_grid_pos + DELTAS[direction], grid_size):
-			direction = randi() % 4
-		initializer_die.move(direction)
-		initializer_die_grid_pos += DELTAS[direction]
-		grid[initializer_die_grid_pos.y][initializer_die_grid_pos.x] += initializer_die.top
-		TileMap.set_cellv(initializer_die_grid_pos, WHITE_TILE)
-		debug_print_grid()
-		
+    for y in range(grid_size):
+        var row = []
+        for x in range(grid_size):
+            row.append(0)
+            TileMap.set_cell(x, y, BLACK_TILE)
+        grid.append(row)
+    var initializer_die = MutableDieState.new()
+    var initializer_die_grid_pos = Vector2(0, 0)
+    for _i in range(n_steps):
+        var direction = randi() % 4
+        while not in_bounds(initializer_die_grid_pos + DELTAS[direction], grid_size):
+            direction = randi() % 4
+        initializer_die.move(direction)
+        initializer_die_grid_pos += DELTAS[direction]
+        grid[initializer_die_grid_pos.y][initializer_die_grid_pos.x] += initializer_die.top
+        TileMap.set_cellv(initializer_die_grid_pos, WHITE_TILE)
+        debug_print_grid()
+
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
