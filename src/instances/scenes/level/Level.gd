79e5a57558d88454539b7ba3627e332ffe070383
Nathaniel Fitzenrider <nathaniel@fitzenrider.com>
2022-07-16 21:36:39 -0400

    Center camera

--- src/instances/scenes/level/Level.gd
+++ src/instances/scenes/level/Level.gd
@@ -52,6 +52,10 @@ func debug_print_grid():
     print()
 
 func initialize_grid(grid_size, n_steps):
+
+    var tmp = round(grid_size / 2.0)
+    Camera.position = Vector2(tmp, tmp)
+
     for y in range(grid_size):
         var row = []
         for x in range(grid_size):
