e47f54aa8271ac52c0fa5eb11060a32a508beea7
hdschnei <hdschnei@ncsu.edu>
2022-07-17 13:26:46 -0400

    added drop sound

--- src/instances/objects/DieIn.gd
+++ src/instances/objects/DieIn.gd
@@ -1,6 +1,9 @@
 extends AnimatedSprite
 
 onready var Die = preload("res://instances/objects/Die.tscn");
+onready var Sound = preload("res://instances/objects/Sound.tscn");
+onready var drop = preload("res://assets/sounds/drop.wav");
+#var hit = false;
 
 var fast = false;
 
@@ -9,8 +12,16 @@ func _ready():
         self.frame = 8;
     else:
         self.frame = 0;
+        
+#func _process(_delta):
+    #if(frame >= 12 && hit == false):
+    #    hit = true;
 
 func _on_DieIn_animation_finished():
     var die = Die.instance();
-    $"..".add_child(die);
+    $"..".add_child(die); 
+    var sound = Sound.instance();
+    sound.stream = drop;
+    $"..".add_child(sound);
     queue_free();
+   
